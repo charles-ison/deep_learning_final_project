54bae4526561b2cc72da175dee5bf3b4d3bc7c90
John Zontos <[jczontos@gmail.com]>
2023-06-08 18:18:08 -0700

    train_model working but very slow.

---
+++ job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -w cn-m-1
+#SBATCH -p cascades
+#SBATCH -A cascades
+#SBATCH --job-name=stemgen
+#SBATCH -t 3-00:00:00
+#SBATCH -c 8
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:2
+#SBATCH --export=ALL
+
+#SBATCH -o logs/train_model.out
+#SBATCH -e logs/train_model.err
+
+# load any software environment module required for app (e.g. matlab, gcc, cuda)
+module load python/3.10 cuda/11.7 sox
+
+# load env
+source env/bin/activate
+
+# run model
+python train_model.py
--- train_model.py
+++ train_model.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
+import tqdm as tq
+
 from modules.audio_transformer import AudioTransformer
 from modules.audio_transformer_decoder import AudioTransformerDecoder
 from audiolm_pytorch.encodec import EncodecWrapper
@@ -14,29 +16,6 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 resample_rate = 24000
 
-
-# ---------- Dataset ----------
-train_data_dir = 'data/mini/train'
-train_dataset = TrackDataset(train_data_dir)
-train_dataset.set_window_size(10)
-train_dataset.set_sample_rate(24000)
-
-train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-# -----------------------------
-
-# ---------- models ----------
-mert_processor = Wav2Vec2FeatureExtractor.from_pretrained("m-a-p/MERT-v1-95M",trust_remote_code=True)
-mert = AutoModel.from_pretrained("m-a-p/MERT-v1-95M", trust_remote_code=True)
-encodec = EncodecWrapper().to(device)
-# -----------------------------
-
-# get sizes
-r, t = train_dataset[0]
-get_tokens(r, t, mert_processor, mert, encodec, resample_rate, device)
-enc_vocab_size = r.shape[-1]
-dec_vocab_size = t.shape[-1]
-max_len = max(r.shape[1], t.shape[1])
-
 # ---------- params ----------
 hidden_dim = 256
 dim_model = 512
@@ -44,33 +23,64 @@ num_layers = 4
 num_heads = 4
 dropout = 0.1
 num_epochs = 10
+
+batch_size=2
+lr=0.00001
 # -----------------------------
 
-# Instantiate the model
-model = AudioTransformer(enc_vocab_size, dec_vocab_size, max_len, dim_model, hidden_dim, num_layers, num_heads, dropout).to(device)
-# model = AudioTransformerDecoder(enc_vocab_size, dec_vocab_size, max_len, dim_model, hidden_dim, num_layers, num_heads, dropout).to(device)
+# ---------- Dataset ----------
+train_data_dir = 'data/mini/train'
+train_dataset = TrackDataset(train_data_dir)
+train_dataset.set_window_size(10)
+train_dataset.set_sample_rate(24000)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+print("INFO: Dataset loaded. Length:", len(train_dataset))
+# -----------------------------
+
+# ---------- models ----------
+mert_processor = Wav2Vec2FeatureExtractor.from_pretrained("m-a-p/MERT-v1-95M",trust_remote_code=True)
+mert = AutoModel.from_pretrained("m-a-p/MERT-v1-95M", trust_remote_code=True)
+encodec = EncodecWrapper().to(device)
+print("INFO: Pretrained models loaded.")
+# -----------------------------
+
+# get sizes
+res, tgt = train_dataset[0]
+semantic_tokens, acoustic_tokens, tgt_tokens = get_tokens(res, tgt, mert_processor, mert, encodec, resample_rate, device)
+
+encoder_input = torch.cat((acoustic_tokens, semantic_tokens), 2).to(device)
+decoder_input = tgt_tokens[:, :-1]
+decoder_output = tgt_tokens[:, 1:]
+
+enc_vocab_size = encoder_input.shape[-1]
+dec_vocab_size = decoder_input.shape[-1]
+max_len = max(encoder_input.shape[1], decoder_input.shape[1])
+
+
+
+# Instantiate the model
+# model = AudioTransformer(enc_vocab_size, dec_vocab_size, max_len, dim_model, hidden_dim, num_layers, num_heads, dropout).to(device)
+model = AudioTransformerDecoder(enc_vocab_size, dec_vocab_size, max_len, dim_model, hidden_dim, num_layers, num_heads, dropout).to(device)
+print("INFO: Model created:", model)
+
+optimizer = torch.optim.Adam(model.parameters(), lr)
 criterion = nn.MSELoss()
 
 
 for epoch in range(num_epochs):
+    pbar = tq.tqdm(desc="Epoch {}".format(epoch+1), total=len(train_loader), unit="steps")
     for i, (residual_audio, tgt_audio) in enumerate(train_loader):
         # -------- get tokens ---------
-        residual_audio = residual_audio.squeeze()
-        tgt_audio = tgt_audio.squeeze()
-        print("residual_audio:", residual_audio.shape)
-        print("tgt_audio:", tgt_audio.shape)
+        # Ensure residual_audio and tgt_audio have batch dimension
+        residual_audio = residual_audio if residual_audio.dim() <= 2 else residual_audio.squeeze()
+        tgt_audio = tgt_audio if tgt_audio.dim() <= 2 else tgt_audio.squeeze()
 
-        semantic_tokens, acoustic_tokens, tgt_tokens = get_tokens(residual_audio.squeeze(), tgt_audio.squeeze(), mert_processor, mert, encodec, resample_rate, device)
+        semantic_tokens, acoustic_tokens, tgt_tokens = get_tokens(residual_audio, tgt_audio, mert_processor, mert, encodec, resample_rate, device)
         # -----------------------------
         encoder_input = torch.cat((acoustic_tokens, semantic_tokens), 2).to(device)
         decoder_input = tgt_tokens[:, :-1]
         decoder_output = tgt_tokens[:, 1:]
-
-        print("encoder_input.shape:", encoder_input.shape)
-        print("decoder_input.shape:", decoder_input.shape)
-        print("decoder_output.shape:", decoder_output.shape)
 
         optimizer.zero_grad()
         predicted_codes = model(encoder_input, decoder_input)
@@ -78,4 +88,6 @@ for epoch in range(num_epochs):
         loss.backward(retain_graph=True)
         optimizer.step()
 
-        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
+        pbar.update(1)
+    pbar.close()
+    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
