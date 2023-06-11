ff64dea5d4dd53a15d2c566f78bd62b6402a1e3c
Charles Ison <isonc@dgx2-3.hpc.engr.oregonstate.edu>
2023-06-11 16:58:55 -0700

    Adding start tokens.

--- modules/audio_transformer.py
+++ modules/audio_transformer.py
@@ -1,9 +1,13 @@
+import torch
 import torch.nn as nn
 from modules.positional_encoding import PositionalEncoding
 
 class AudioTransformer(nn.Module):
     def __init__(self, encoder_input_size, decoder_input_size, max_len, dim_val, hidden_dim, num_layers, num_heads, dropout):
         super(AudioTransformer, self).__init__()
+
+        self.max_len = max_len
+        self.start_token = nn.Parameter(torch.randn(1, 1, dim_val))
 
         self.encoder_input_layer = nn.Linear(
             in_features=encoder_input_size, 
@@ -29,10 +33,17 @@ class AudioTransformer(nn.Module):
     def forward(self, src, tgt, tgt_mask=None):
         src = self.encoder_input_layer(src)
         src = self.positional_encoding(src)
+        start_tokens = self.start_token.repeat(src.shape[0], 1, 1)
+        src = torch.cat((start_tokens, src), dim=1)
+
         tgt = self.decoder_input_layer(tgt)
         tgt = self.positional_encoding(tgt)
+        tgt = torch.cat((start_tokens, tgt), dim=1)
 
         transformer_output = self.transformer(src, tgt, tgt_mask)
         output = self.linear_mapping(transformer_output)
+
+        # Removing end token
+        output = output[:, 0:self.max_len, :]
 
         return output
--- modules/audio_transformer_decoder.py
+++ modules/audio_transformer_decoder.py
@@ -1,9 +1,13 @@
+import torch
 import torch.nn as nn
 from modules.positional_encoding import PositionalEncoding
 
 class AudioTransformerDecoder(nn.Module):
     def __init__(self, mem_input_size, tgt_input_size, max_len, dim_val, hidden_dim, num_layers, num_heads, dropout):
         super(AudioTransformerDecoder, self).__init__()
+
+        self.max_len = max_len
+        self.start_token = nn.Parameter(torch.randn(1, 1, dim_val))
 
         self.mem_fc = nn.Linear(
             in_features=mem_input_size,
@@ -28,14 +32,21 @@ class AudioTransformerDecoder(nn.Module):
         self.transformer_decoder = nn.TransformerDecoder(transformer_decoder_layer, num_layers=num_layers)
 
     def forward(self, mem, tgt, tgt_mask=None):
+
         tgt = self.tgt_fc(tgt)
         tgt = self.positional_encoding(tgt)
+        start_tokens = self.start_token.repeat(tgt.shape[0], 1, 1)
+        tgt = torch.cat((start_tokens, tgt), dim = 1)
 
         mem = self.mem_fc(mem)
         mem = self.positional_encoding(mem)
+        mem = torch.cat((start_tokens, mem), dim=1)
 
         # Note, the decoder switches the order these need to be passed from the vanilla transformer (encoder and decoder)
         transformer_output = self.transformer_decoder(tgt, mem, tgt_mask)
         output = self.fc_output(transformer_output)
+
+        # Removing end token
+        output = output[:, 0:self.max_len, :]
 
         return output
