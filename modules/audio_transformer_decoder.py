2a4c200e9f7a875af5898771904f064501589286
John Zontos <[jczontos@gmail.com]>
2023-06-12 18:57:10 -0700

    model updated still failing on loss call

--- modules/audio_transformer_decoder.py
+++ modules/audio_transformer_decoder.py
@@ -3,50 +3,58 @@ import torch.nn as nn
 from modules.positional_encoding import PositionalEncoding
 
 class AudioTransformerDecoder(nn.Module):
-    def __init__(self, mem_input_size, tgt_input_size, max_len, dim_val, hidden_dim, num_layers, num_heads, dropout):
+    def __init__(self, mem_input_size, tgt_voc_size, max_len, embedding_dim, num_q, hidden_dim, num_layers, num_heads, dropout):
         super(AudioTransformerDecoder, self).__init__()
-
+        dim_model = embedding_dim * num_q
+        self.dim_model = dim_model
         self.max_len = max_len
-        self.start_token = nn.Parameter(torch.randn(1, 1, dim_val))
+        self.num_q = num_q
+        self.tgt_voc_size = tgt_voc_size
+        self.start_token = nn.Parameter(torch.randn(1, 1, dim_model))
 
-        self.mem_fc = nn.Linear(
-            in_features=mem_input_size,
-            out_features=dim_val
-        )
-
-        self.tgt_fc = nn.Linear(
-            in_features=tgt_input_size,
-            out_features=dim_val
-        )
+        # embedding layers
+        self.tgt_embedding = nn.Embedding(tgt_voc_size, embedding_dim)
         
-        self.fc_output = nn.Linear(
-            in_features=dim_val, 
-            out_features=tgt_input_size
-        )
+        self.mem_fc = nn.Linear(mem_input_size, dim_model)
 
-        self.positional_encoding = PositionalEncoding(d_model=dim_val, dropout=dropout, max_len=max_len)
+        self.pe = PositionalEncoding(d_model=dim_model, dropout=dropout, max_len=max_len)
 
-        transformer_decoder_layer = nn.TransformerDecoderLayer(d_model=dim_val, nhead=num_heads, dim_feedforward=hidden_dim,
+        transformer_decoder_layer = nn.TransformerDecoderLayer(d_model=dim_model, nhead=num_heads, dim_feedforward=hidden_dim,
                                                                dropout=dropout, batch_first=True, norm_first=True)
 
         self.transformer_decoder = nn.TransformerDecoder(transformer_decoder_layer, num_layers=num_layers)
 
+        self.fc_output = nn.Linear(in_features=dim_model, out_features=tgt_voc_size * num_q)
+
+        self.log_softmax = nn.LogSoftmax(dim=-1)
+
+
     def forward(self, mem, tgt, tgt_mask=None):
+        # start_tokens = self.start_token.repeat(tgt.shape[0], 1, 1)
+        # mem = torch.cat((start_tokens, mem), dim = 1)
+        # tgt = torch.cat((start_tokens, tgt), dim = 1)
 
-        tgt = self.tgt_fc(tgt)
-        tgt = self.positional_encoding(tgt)
-        start_tokens = self.start_token.repeat(tgt.shape[0], 1, 1)
-        tgt = torch.cat((start_tokens, tgt), dim = 1)
+        # mem.shape == [Batchsize, samples, mem_input_size]
+        # tgt.shape == [Batchsize, samples, num_q]
 
-        mem = self.mem_fc(mem)
-        mem = self.positional_encoding(mem)
-        mem = torch.cat((start_tokens, mem), dim=1)
-
-        # Note, the decoder switches the order these need to be passed from the vanilla transformer (encoder and decoder)
+        mem, tgt = self.mem_fc(mem), self.tgt_embedding(tgt.int())
+        print("mem.shape:", mem.shape)
+        print("tgt.shape:", tgt.shape)
+            # mem = [Batchsize, samples, dim_model]
+            # tgt = [Batchsize, samples, num_q, embedding_dim]
+        tgt = torch.flatten(tgt, start_dim=2)
+        print("tgt.shape:", tgt.shape)
+            # tgt = [Batchsize, samples, dim_model]
+        mem, tgt = self.pe(mem), self.pe(tgt)
+            # tgt = [Batchsize, samples, dim_model]        
+        
         transformer_output = self.transformer_decoder(tgt, mem, tgt_mask)
         output = self.fc_output(transformer_output)
+
+        # unflatten out
+        output = torch.unflatten(output, -1, (self.num_q, self.tgt_voc_size))
 
         # Removing end token
         output = output[:, 0:self.max_len, :]
 
-        return output
+        return self.log_softmax(output)
--- train_model.py
+++ train_model.py
@@ -24,17 +24,19 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # ---------- params ----------
 hidden_dim = 256
 dim_model = 512
+embedding_dim = 10
 num_layers = 4
 num_heads = 4
 dropout = 0.1
 num_epochs = 1
 resample_rate = 24000
-batch_size=48
+batch_size=4            # must be divisable by num_gpu
 lr=1e-5
 # -----------------------------
 
 # ---------- Dataset ----------
-train_data_dir = '/nfs/hpc/share/stemgen/slakh2100_wav_redux/test'
+# NOTE: developing on babyset need to change for training.
+train_data_dir = '/nfs/hpc/share/stemgen/babyslakh_16k'
 train_dataset = TrackDataset(train_data_dir)
 train_dataset.set_window_size(5)
 train_dataset.set_sample_rate(resample_rate)
@@ -49,6 +51,8 @@ mert_processor = Wav2Vec2FeatureExtractor.from_pretrained("m-a-p/MERT-v1-95M", t
 # TODO: Look into checnging sequence length.
 mert = AutoModel.from_pretrained("m-a-p/MERT-v1-95M", trust_remote_code=True)
 encodec = EncodecWrapper().to(device)
+codebook_size = 1024
+num_q = encodec.num_quantizers
 print("INFO: Pretrained models loaded.")
 # -----------------------------
 
@@ -60,15 +64,14 @@ semantic_tokens, acoustic_tokens, tgt_tokens = get_tokens(res, tgt, mert_process
 
 src = torch.cat((acoustic_tokens, semantic_tokens), 2).to(device)
 tgt = tgt_tokens[:, :-1]
-decoder_output = tgt_tokens[:, 1:]
+# decoder_output = tgt_tokens[:, 1:]
 
-enc_vocab_size = src.shape[-1]
-dec_vocab_size = tgt.shape[-1]
+src_emb_dim = src.shape[-1]
 max_len = max(src.shape[1], tgt.shape[1])
 
 # Instantiate the model
 #model = AudioTransformer(enc_vocab_size, dec_vocab_size, max_len, dim_model, hidden_dim, num_layers, num_heads, dropout).to(device)
-model = AudioTransformerDecoder(enc_vocab_size, dec_vocab_size, max_len, dim_model, hidden_dim, num_layers, num_heads, dropout).to(device)
+model = AudioTransformerDecoder(src_emb_dim, codebook_size, max_len, embedding_dim, num_q, hidden_dim, num_layers, num_heads, dropout).to(device)
 print("INFO: Model created:", model)
 
 if torch.cuda.device_count() > 1:
@@ -76,12 +79,11 @@ if torch.cuda.device_count() > 1:
     model = nn.DataParallel(model)
 
 optimizer = torch.optim.Adam(model.parameters(), lr)
-
-# TODO: @jc investigate correct loss function. (try CrossEntropyLoss and prediction codebook logits)
-criterion = nn.NLLLoss()
+criterion = nn.CrossEntropyLoss()
 
 best_loss = None
-tgt_mask = nn.Transformer.generate_square_subsequent_mask(max_len + 1, device=device)
+# add back max_len + 1 when start token is fixed.
+tgt_mask = nn.Transformer.generate_square_subsequent_mask(max_len, device=device)
 tgt_mask = tgt_mask.unsqueeze(dim=0)
 tgt_mask = tgt_mask.repeat(num_heads * batch_size, 1, 1)
 print("tgt_mask.shape:", tgt_mask.shape)
@@ -91,16 +93,16 @@ for epoch in range(num_epochs):
     for i, (residual_audio, tgt_audio) in enumerate(train_loader):
         # -------- get tokens ---------
         semantic_tokens, acoustic_tokens, tgt_tokens = get_tokens(residual_audio, tgt_audio, mert_processor, mert, encodec, resample_rate, device)
-        # -----------------------------
-        src = torch.cat((acoustic_tokens, semantic_tokens), 2).to(device)
+        mem = torch.cat((acoustic_tokens, semantic_tokens), 2).to(device)
         tgt = tgt_tokens[:, :-1]            # [B, timesteps-1, num_quantizers=8]
-        tgt_tokens = tgt_tokens[:, 1:]      # [B, timesteps-1, num_quantizers=8]
+        gt_tgt = tgt_tokens[:, 1:]          # [B, timesteps-1, num_quantizers=8]
 
         optimizer.zero_grad()
         # NOTE: removed mask for testing put back later.
-        predicted_codes = model(src, tgt, tgt_mask=None)
-
-        loss = criterion(predicted_codes, tgt_tokens)
+        print("input mem.shape:", mem.shape)
+        print("input tgt.shape:", tgt.shape)
+        predicted_codes = model(mem, tgt, tgt_mask=tgt_mask)  # should be [B, L, Q, V]
+        loss = criterion(predicted_codes, gt_tgt)
         if NEPTUNE_SWITCH == 1:
             runtime['train/loss'].log(loss)
         loss.backward(retain_graph=True)
@@ -111,5 +113,5 @@ for epoch in range(num_epochs):
     print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
     if not best_loss or loss < best_loss:
         # NOTE: look at alternative model saving strat
-        torch.save(model, model.__class__.__name__ + "_saved_model.pt")
+        torch.save(model, "model.pt")
         best_loss = loss
